2d070b0f9f3c09e5669be153b14b34db92124efc
zula-n <81601949+zula-n@users.noreply.github.com>
2021-04-05 20:35:37 -0400

    Add files via upload

---
+++ errors.c
@@ -0,0 +1,11 @@
+//
+// Created by zula nyamtur on 4/2/21.
+//
+
+char errorMessages[] = {
+        "Calculation successful",
+        "Encountered null pointer",
+        "Too many operands",
+        "Too few operands"
+}
+
---
+++ errors.h
@@ -0,0 +1,14 @@
+//
+// Created by zula nyamtur on 3/30/21.
+//
+
+#ifndef PS4_ERRORS_H
+#define PS4_ERRORS_H
+
+#define   SUCCESS  0
+#define   NULLPTR   1
+#define   TOOMANY 2
+#define   TOOFEW    3
+
+
+#endif //PS4_ERRORS_H
